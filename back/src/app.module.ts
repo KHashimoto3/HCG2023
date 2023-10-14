6b2ef9e39f2f20ab2595241c54e60a1bfed88031
KHashimoto3 <hashi.koki385@gmail.com>
2023-10-14 11:11:55 +0900

    programのモジュール群を作成した。

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProgramController } from './program/program.controller';
+import { ProgramService } from './program/program.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProgramController],
+  providers: [AppService, ProgramService],
 })
 export class AppModule {}
