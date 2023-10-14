55bba397b628a26b9e4bb970fbcbd58a73fca017
KHashimoto3 <hashi.koki385@gmail.com>
2023-10-14 12:09:44 +0900

    exec apiで文字を返せるようにした。

--- back/src/program/program.controller.ts
+++ back/src/program/program.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ExecInputDto } from './dto/exec-input.dto';
 
 @Controller('program')
 export class ProgramController {
     @Get('hello')
     hello(): {message: string} {
         return {message: "hello program api!"};
+    }
+    @Post('exec')
+    execCode(@Body() execInputDto: ExecInputDto): {message: string} {
+        return {message: "hello exec api!"};
     }
 }
