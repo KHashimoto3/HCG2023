4fadc82703a6348e2cb7bf9e2a0fad0b0e667d83
KHashimoto3 <hashi.koki385@gmail.com>
2023-10-16 12:17:15 +0900

    コード入力用のテキストエリアを配置した。

--- front/src/components/features/CodeCheckInput.tsx
+++ front/src/components/features/CodeCheckInput.tsx
@@ -5,6 +5,11 @@ export const CodeCheckInput = () => {
     <>
       <Container maxWidth="md">
         <h1>左側のコンポーネントです。</h1>
+        <textarea
+          rows={20}
+          cols={45}
+          style={{ fontSize: "18pt", borderRadius: "10px" }}
+        />
       </Container>
     </>
   );
