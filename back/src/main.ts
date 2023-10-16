469a8e018a4d5266f5697f70a42752fbad6edf2b
KHashimoto3 <hashi.koki385@gmail.com>
2023-10-16 18:14:25 +0900

    exec apiにcorsの設定を書いた。

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,7 +4,25 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe);
+  app.enableCors();
+
+  //const expressApp = app.getHttpAdapter().getInstance();
+
+  //corsの設定（localhost:5173からのアクセスを許可）
+  /*expressApp.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+  });*/
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
