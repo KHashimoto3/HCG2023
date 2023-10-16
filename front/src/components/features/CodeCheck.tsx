496144e876063fbf40f0160a85cd4c7ee0e2f6e9
KHashimoto3 <hashi.koki385@gmail.com>
2023-10-16 11:52:48 +0900

    MUIのGridを使って２段組に分割した。

--- front/src/components/features/CodeCheck.tsx
+++ front/src/components/features/CodeCheck.tsx
@@ -1,11 +1,18 @@
+import { Grid } from "@mui/material";
 import { CodeCheckInput } from "./CodeCheckInput";
 import { CodeCheckList } from "./CodeCheckList";
 
 export const CodeCheck = () => {
   return (
     <>
-      <CodeCheckInput />
-      <CodeCheckList />
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <CodeCheckInput />
+        </Grid>
+        <Grid item xs={6}>
+          <CodeCheckList />
+        </Grid>
+      </Grid>
     </>
   );
 };
