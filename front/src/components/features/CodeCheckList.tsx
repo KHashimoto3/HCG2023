93da582476bfa2c9168a28c01bcc155b22898de4
KHashimoto3 <hashi.koki385@gmail.com>
2023-10-16 12:21:57 +0900

    ヒントの提示エリアを設置した。

--- front/src/components/features/CodeCheckList.tsx
+++ front/src/components/features/CodeCheckList.tsx
@@ -1,10 +1,23 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 
 export const CodeCheckList = () => {
   return (
     <>
       <Container maxWidth="md">
         <h1>右側のコンポーネントです。</h1>
+        <Box
+          sx={{
+            justifyContent: "center",
+            borderRadius: "10px",
+            border: "1px solid black",
+            padding: "10px",
+            height: "400px",
+          }}
+        >
+          <p>テキスト1</p>
+          <p>テキスト2</p>
+          <p>テキスト3</p>
+        </Box>
       </Container>
     </>
   );
