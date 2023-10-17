73aeecc662059dd5a799987d19e525f2327f9a7d
KHashimoto3 <hashi.koki385@gmail.com>
2023-10-17 14:51:47 +0900

    エラーの解決法を取得できるようにした。

--- front/src/components/features/CodeCheck.tsx
+++ front/src/components/features/CodeCheck.tsx
@@ -12,31 +12,86 @@ export const CodeCheck = () => {
   const [checkButtonDisabled, setCheckButtonDisabled] =
     useState<boolean>(false);
 
-  const checkCode = () => {
+  const checkCode = async () => {
     setCheckButtonDisabled(true);
     console.log("チェックします：" + code);
-    const checkCodeResult = execCode();
-    //dataがExeqResult型でない場合はreturnする
-    if (!("status" in checkCodeResult) || !("error" in checkCodeResult)) {
-      alert("方チェックのエラーが発生しました");
+
+    let execResult: ExecResult;
+    let errorResolve: ErrorResolve;
+
+    //exec apiに接続して、codeとinputを送信する
+    try {
+      //codeが殻の場合はエラーを返す
+      if (code === "") {
+        alert("コードが入力されていません");
+        return;
+      }
+      let dataObj = {
+        code: code,
+        input: codeInput,
+      };
+      //inputが殻の場合は、inputにnoneを入れる
+      if (codeInput === "") {
+        dataObj.input = "none";
+      }
+      const url = "http://localhost:3000/program/exec";
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataObj),
+      });
+      //ステータスコードが200以外の場合はエラーを返す
+      /*if (response.status !== 200) {
+        alert("コードの実行時にエラーが発生しました。200以外");
+        const data = {
+          status: "exit",
+          error: ["コードの実行時にエラーが発生しました。"],
+        };
+        execResult = data;
+        setCheckButtonDisabled(false);
+        return;
+      }*/
+      execResult = await response.json();
+      console.log("実行結果" + execResult.status + "エラー" + execResult.error);
+    } catch (error) {
+      alert("コードの実行時にエラーが発生しました。catch");
       setCheckButtonDisabled(false);
       return;
     }
 
-    console.log(
-      "実行結果：" + checkCodeResult.status + "エラー：" + checkCodeResult.error
-    );
-
-    const errorResolve = getErrorResolve(checkCodeResult.error);
-
-    console.log("エラーの対処法：" + errorResolve);
+    if (execResult.error.length >= 1) {
+      //error-resolve apiに接続して、エラーの対処法を受け取る
+      try {
+        const url = "http://localhost:3000/program/error-resolve";
+        const dataObj = {
+          errors: execResult.error,
+        };
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(dataObj),
+        });
+        errorResolve = await response.json();
+        console.log("エラーの解決法のリスト" + errorResolve);
+        console.log(errorResolve);
+      } catch (error) {
+        alert("エラーの解決法の取得時にエラーが発生しました。");
+        setCheckButtonDisabled(false);
+        return;
+      }
+    }
 
     setCheckButtonDisabled(false);
   };
 
   // exec apiに接続して、codeとinputを送信する
   // その結果をsetExecResultListに入れる
-  const execCode = async () => {
+  /*const execCode = async () => {
     try {
       //codeが殻の場合はエラーを返す
       if (code === "") {
@@ -97,7 +152,7 @@ export const CodeCheck = () => {
     } catch (error) {
       console.error(error);
     }
-  };
+  };*/
 
   return (
     <>
