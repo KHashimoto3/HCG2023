974aecf6413ac9da38c6a7467bbfac6ef92cb82b
KHashimoto3 <hashi.koki385@gmail.com>
2023-10-26 14:45:40 +0900

    エラーやミスがない場合は、その旨を表示するようにした。

--- front/src/components/features/CheckResult.tsx
+++ front/src/components/features/CheckResult.tsx
@@ -9,34 +9,38 @@ type Props = {
 export const CheckResult = (props: Props) => {
   const { errorResolveList } = props;
   const { foundMissList } = props;
-  return (
-    <>
-      {errorResolveList.map((errorResolve, index) => (
-        <>
-          <div key={index}>
-            <p>必ず修正！</p>
-            <p>
-              {errorResolve.row}行目 {errorResolve.column}文字目
-            </p>
-            <p>{errorResolve.description}</p>
-            <p>{errorResolve.error}</p>
-            <p>{errorResolve.method}</p>
-            <hr />
-          </div>
-        </>
-      ))}
-      {foundMissList.map((foundMiss, index) => (
-        <>
-          <div key={index}>
-            <p>確認</p>
-            <p>
-              {foundMiss.row}行目 {foundMiss.column}文字目
-            </p>
-            <p>{foundMiss.description}</p>
-            <hr />
-          </div>
-        </>
-      ))}
-    </>
-  );
+  if (errorResolveList.length === 0 && foundMissList.length === 0) {
+    return <p>おめでとうございます。問題は見つかりませんでした。</p>;
+  } else {
+    return (
+      <>
+        {errorResolveList.map((errorResolve, index) => (
+          <>
+            <div key={index}>
+              <p>必ず修正！</p>
+              <p>
+                {errorResolve.row}行目 {errorResolve.column}文字目
+              </p>
+              <p>{errorResolve.description}</p>
+              <p>{errorResolve.error}</p>
+              <p>{errorResolve.method}</p>
+              <hr />
+            </div>
+          </>
+        ))}
+        {foundMissList.map((foundMiss, index) => (
+          <>
+            <div key={index}>
+              <p>確認</p>
+              <p>
+                {foundMiss.row}行目 {foundMiss.column}文字目
+              </p>
+              <p>{foundMiss.description}</p>
+              <hr />
+            </div>
+          </>
+        ))}
+      </>
+    );
+  }
 };
